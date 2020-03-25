930fc1a1bcfb100219c674d7d0a1b839dd8994f9
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-26 02:54:00 +0900

    updage comments component

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectLoading,
+  selectError,
+  selectComments,
+  fetchComments,
+} from '../store/commentSlice';
 
 function Comments() {
+  const dispatch = useDispatch();
+
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+  const comments = useSelector(selectComments);
+
+  useEffect(() => {
+    dispatch(fetchComments());
+  }, [dispatch]);
+
+  if (loading) return <p>...loading</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Comments</h1>
+      {comments.map(comment => (
+        <div key={comment.id}>
+          <p>{comment.created_at}</p>
+          <pre>{comment.body}</pre>
+        </div>
+      ))}
     </div>
   );
 }
