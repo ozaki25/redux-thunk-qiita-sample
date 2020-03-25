1e4364893f5ea04a5cadf0c629d9db0e9e790bdf
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-26 02:53:17 +0900

    apply redux to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
 import AddComment from './components/AddComment';
 import Comments from './components/Comments';
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <AddComment />
       <Comments />
-    </div>
+    </Provider>
   );
 }
 
